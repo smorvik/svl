e19f42557461d891adf332e174480e8d8f092f62
Гость <Guest@iMac-Mac005-6.local>
2022-04-16 14:59:37 +0900

    qwertyqw

--- svl/ViewController.swift
+++ svl/ViewController.swift
@@ -8,9 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var bAC: UIButton!
+    @IBOutlet weak var b7: UIButton!
+    @IBOutlet weak var b8: UIButton!
+    @IBOutlet weak var b9: UIButton!
+    @IBOutlet weak var b4: UIButton!
+    @IBOutlet weak var b5: UIButton!
+    @IBOutlet weak var b6: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var b0: UIButton!
+    @IBOutlet weak var delit: UIButton!
+    @IBOutlet weak var ymnj: UIButton!
+    @IBOutlet weak var min: UIButton!
+    @IBOutlet weak var plus: UIButton!
+    @IBOutlet weak var ravno: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bAC.layer.cornerRadius = 36
+        b1.layer.cornerRadius = 36
+        b2.layer.cornerRadius = 36
+        b3.layer.cornerRadius = 36
+        b4.layer.cornerRadius = 36
+        b5.layer.cornerRadius = 36
+        b6.layer.cornerRadius = 36
+        b7.layer.cornerRadius = 36
+        b8.layer.cornerRadius = 36
+        b9.layer.cornerRadius = 36
+        b0.layer.cornerRadius = 36
+        delit.layer.cornerRadius = 36
+        ymnj.layer.cornerRadius = 36
+        min.layer.cornerRadius = 36
+        plus.layer.cornerRadius = 36
+        ravno.layer.cornerRadius = 36
         // Do any additional setup after loading the view.
     }
 
